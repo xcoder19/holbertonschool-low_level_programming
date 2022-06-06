6d2651972c20b897448ea28c6256d4ec4a761795
hedi <hedihamida98@gmail.com>
2022-06-06 11:38:57 +0100

    hello

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - execute file
+ * Return: 0
+ */
+int main(void)
+{
+char str[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", str);
+return (0);
+}
