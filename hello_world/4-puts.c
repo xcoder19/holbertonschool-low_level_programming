28efa219d2ac32c3717d3988ca53a78a23c644e1
hedi <hedihamida98@gmail.com>
2022-06-06 10:44:02 +0100

    hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() { 
-    char *str = "\"Programming is like building a multilingual puzzle";
-    puts(str);
-    return 0;
+int main(void)
+{
+char *str = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
