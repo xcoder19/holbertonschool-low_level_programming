c8362d8d3d0ec30172b2e92c16d2980e972c0b16
hedi <hedihamida98@gmail.com>
2022-06-07 22:09:12 +0100

    hello

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 v = n % 10;
 if (v > 5)
 {
-printf("last digit of %d is %d and is greater than 5", n, v);
+printf("last digit of %d is %d and is greater than 5\n", n, v);
 }
 else if (v == 0)
 {
-printf("last digit of %d is %d and is 0", n, v);
+printf("last digit of %d is %d and is 0\n", n, v);
 }
 else if (v < 6 && v != 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0", n, v);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, v);
 }
 return (0);
 }
