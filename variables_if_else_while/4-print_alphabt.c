c1e8ece3fb5b6289c1cea7cd07c1fb545b1caf19
hedi <hedihamida98@gmail.com>
2022-06-07 22:41:43 +0100

    hello

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - execution
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == 'q' && i == "e")
+{
+i = "";
+};
+putchar(i);
+};
+putchar('\n');
+return (0);
+}
