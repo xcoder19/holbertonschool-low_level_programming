aa902842ee4bed67cc950749caee27090e5fcfde
hedi <hedihamida98@gmail.com>
2022-06-07 12:52:43 +0100

    hello

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - execute file
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)", sizeof(char));
+printf("Size of a int: %d byte(s)", sizeof(int));
+printf("Size of a long: %d byte(s)", sizeof(char));
+printf("Size of a long long: %d byte(s)", sizeof(long long));
+printf("Size of a float: %d byte(s)", sizeof(float));
+return (0);
+}
