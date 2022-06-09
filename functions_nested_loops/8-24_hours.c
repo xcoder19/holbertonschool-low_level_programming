0058070f895a79cc97ca157f0c6fdc9e7caeb237
hedi <hedihamida98@gmail.com>
2022-06-09 13:32:41 +0100

    hello

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * jack_bauer - prints hours
+
+ * Return: On success the last digit of a number.
+ */
+
+void jack_bauer(void)
+{
+
+int i, k;
+
+for (i = 0; i <= 23; i++)
+{
+for (k = 0; i <= 59; k++)
+{
+putchar('0' + i);
+putchar('0' + k);
+}
+}
+}
