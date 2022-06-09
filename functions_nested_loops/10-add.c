5a234f1f0edafafc7805ff65ef1f587e231e694b
hedi <hedihamida98@gmail.com>
2022-06-09 20:44:29 +0100

    hello

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two ints
+ * @i: integer
+ * @k: integer
+ * Return: On success the sum.
+ */
+
+int add(int i, int k) { return (i + i); }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int print_sign(int n);
 int _abs(int i);
 int print_last_digit(int i);
 void jack_bauer(void);
+int add(int, int);
