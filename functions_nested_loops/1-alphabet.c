a081a2a066cdfc305f5985503108d44b9e56f9b3
hedi <hedihamida98@gmail.com>
2022-06-09 10:08:16 +0100

    hello

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - execution
+ * Return: 0
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+/**
+ * print_alphabet - print alphabet
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
