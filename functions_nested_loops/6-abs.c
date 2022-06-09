67574f875d31b79561cd0e7ecf1da8c336fe2e6f
hedi <hedihamida98@gmail.com>
2022-06-09 12:52:49 +0100

    hello

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - checks
+ * @i: integer
+ * Return: On success int.
+ */
+
+int _abs(int i)
+{
+if (i < 0)
+{
+return (-i);
+}
+else
+{
+return (i);
+}
+}
