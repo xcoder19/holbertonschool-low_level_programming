b57e70da513d6a7b2f1337b0fe796fa62e248655
hedi <hedihamida98@gmail.com>
2022-06-09 20:55:35 +0100

    hello

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print natural number
+ * @n: integer
+
+ * Return: On success the natural numbers.
+ */
+
+void print_to_98(int n)
+{
+int k;
+if (n <= 98)
+{
+for (k = n; k <= 98; k++)
+{
+return (k);
+}
+}
+else if (n < 0)
+{
+for (k = n; k <= 98; k++)
+{
+return (k);
+}
+}
+else
+{
+for (k = n; k >= 98; k++)
+{
+return (k);
+}
+}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int i);
 int print_last_digit(int i);
 void jack_bauer(void);
 int add(int, int);
+void print_to_98(int n);
