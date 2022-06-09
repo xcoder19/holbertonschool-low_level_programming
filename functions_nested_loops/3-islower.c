aeb03baaa5e9642f7c03f32076e9bd0d86dee9ba
hedi <hedihamida98@gmail.com>
2022-06-09 10:44:43 +0100

    hello

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * islower - print alphabet
+ * @c: the character to print
+ * Return: On success 1.
+ */
+
+int _islower(int c)
+{
+
+if (c < 97)
+{
+return (0);
+}
+
+return (1);
+}
