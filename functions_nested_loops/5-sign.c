f7c02de54a679944fcdf26590951ff0078c766f8
hedi <hedihamida98@gmail.com>
2022-06-09 11:09:10 +0100

    hello

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 
 if (n > 0)
 {
-printf('+');
+printf("+");
 return (1);
 }
 else if (n == 0)
 {
-printf('0');
+printf("0");
 return (0);
 }
 else
 {
-printf('-');
+printf("-");
 return (-1);
 }
 
