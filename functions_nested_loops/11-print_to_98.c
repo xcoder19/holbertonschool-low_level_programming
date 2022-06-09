704cf3ca4a823c82dc43a59a991d6f1aad3f5a9b
hedi <hedihamida98@gmail.com>
2022-06-09 21:10:44 +0100

    hello

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,32 +4,13 @@
 /**
  * print_to_98 - print natural number
  * @n: integer
-
  * Return: On success the natural numbers.
  */
 
 void print_to_98(int n)
 {
-int k;
-if (n <= 98)
+
+while (n <= 98)
 {
-for (k = n; k < 98; k++)
-{
-_putchar('0' + k);
-}
-}
-else if (n < 0)
-{
-for (k = n; k <= 98; k++)
-{
-_putchar('0' + k);
-}
-}
-else
-{
-for (k = n; k >= 98; k++)
-{
-_putchar('0' + k);
-}
-}
+_putchar('0' + n--);
 }
