52a57669c2ba4780955f6ddfa8669402d32ffaae
hedi <hedihamida98@gmail.com>
2022-06-09 11:07:36 +0100

    hello

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - checks
+ * @n: the int
+ * Return: On success 1.
+ */
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+
+return (0);
+}
