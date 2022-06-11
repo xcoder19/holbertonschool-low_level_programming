9be6db89dd7fba6d89265dbfcbc5e4d0b1b7cf90
hedi <hedihamida98@gmail.com>
2022-06-11 18:33:16 +0100

    hello

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+int i, k;
+for (k = 0; k <= 10; k++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar('0' + i);
+}
+}
+}
