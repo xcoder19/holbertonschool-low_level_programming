b6027131c4ed82cb4b3515358b697497cbbbdeb7
hedi <hedihamida98@gmail.com>
2022-06-11 19:01:55 +0100

    new

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  print_line - print lines
- * @n: number of _
+ *  print_diagonal - print lines
+ * @n: number of diogonal lines
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
 int i;
 
 for (i = 0; i < n; i++)
 {
-_putchar('_');
+_putchar('/');
 }
 _putchar('\n');
 }
