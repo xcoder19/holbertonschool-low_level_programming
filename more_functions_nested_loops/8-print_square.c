4bd85cb41633fffdf9a892d30178f30ac85824ec
hedi <hedihamida98@gmail.com>
2022-06-11 19:48:08 +0100

    new

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,14 +4,10 @@
  *  print_square - print lines
  * @size: number of squares
  */
+
 void print_square(int size)
 {
 int i, k;
-
-if (size <= 0)
-{
-_putchar('\n');
-}
 for (i = 1; i <= size; i++)
 {
 for (k = 1; k <= size; k++)
