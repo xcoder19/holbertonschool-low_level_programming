ad838e0795eac13d01b2ffc1df14dad270c1ffba
hedi <hedihamida98@gmail.com>
2022-06-12 19:02:16 +0100

    new

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - exectution
+ * Return: 0
+ */
+
+int main(void)
+{
+
+int i;
+for (i = 1; i <= 100; i++)
+{
+
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("Buzz");
+}
+else if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf(i);
+}
+_putchar(' ');
+}
+
+return (0);
+}
