083374016dbf5d3641d62a2313928509f6ebeaa9
hedi <hedihamida98@gmail.com>
2022-06-13 11:48:31 +0100

    new

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+#include <string.h>
+/**
+ * _puts
+ * @str: pointer to str
+ */
+void _puts(char *str)
+{
+int i;
+
+unsigned long n;
+n = strlen(str);
+char k[n];
+for (i = 0; i <= n; i++)
+{
+k[i] = *(str + i);
+}
+
+fprintf(stdout, k);
+}
