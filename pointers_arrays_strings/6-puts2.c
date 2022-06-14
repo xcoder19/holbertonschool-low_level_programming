1a49ca63ed82b1cf32fb4af7e58f283840b79917
hedi <hedihamida98@gmail.com>
2022-06-14 13:03:44 +0100

    new

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - puts
+ * @str: pointer of char
+ */
+void puts2(char *str)
+{
+
+int i;
+
+for (i = 0; *(str + i) != '\0'; ++i)
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+}
+}
+}
