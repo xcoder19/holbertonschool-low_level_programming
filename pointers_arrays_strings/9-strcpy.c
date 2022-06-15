27dc18a191a65b130ec200c5a5530940660fb3d9
hedi <hedihamida98@gmail.com>
2022-06-15 09:40:59 +0100

    new

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * *_strcpy - copy string
+ * @dest: pointer char
+ * @src: pointer char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; *(src + i) != '\0'; ++i)
+;
+char arr[i];
+return dest;
+}
