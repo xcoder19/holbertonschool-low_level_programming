2656a5e09a5ec8cd2b9fe7a6d514e81186e03f17
hedi <hedihamida98@gmail.com>
2022-06-15 09:19:44 +0100

    new

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 
 #include "main.h"
 /**
- * _strlen - swapingint
+ * _strlen - counting
  * @s: pointer to the first char of a string
  * Return: length of string
  */
 int _strlen(char *s)
 {
-int i;
-for (i = 0; *(s + i) != '\0'; ++i)
-;
+    int i;
+    for (i = 0; *(s + i) != '\0'; ++i)
+        ;
 
-return (i);
+    return (i);
 }
