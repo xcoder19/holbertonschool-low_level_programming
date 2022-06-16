eac6c96faab21ed6260fd00eb1324a37411bb50b
hedi <hedihamida98@gmail.com>
2022-06-16 14:15:24 +0100

    new

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,10 @@
 
 #include "main.h"
+/**
+ * _strlen - counting
+ * @s: pointer to the first char of a string
+ * Return: length of string
+ */
 int _strlen(char *s)
 {
 	int i;
@@ -8,9 +13,16 @@ int _strlen(char *s)
 
 	return (i);
 }
+/**
+ * _strspn - strspn function
+ * @s: pointer to the first char of a string
+ * @accept: pointer char
+ * Return: i
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
+
 	int k, j;
 
 	for (j = 0; j <= _strlen(s) - 1; j++)
