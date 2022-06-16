c274e7c79bfe2d511b64dc1e3e7021da8c4e30c4
hedi <hedihamida98@gmail.com>
2022-06-16 12:57:52 +0100

    new

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -7,7 +7,26 @@
  * @b: char
  * @n unsigned int
  */
+
+char *_strcat(char *dest, char *src)
+{
+
+	char *ptr = dest + strlen(dest);
+
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	memset(s, b, n);
+	while (n--)
+	{
+		_strcat(s, &b);
+	}
 }
