36992b7e01f084752c6e019c14cb38bda0a2d683
hedi <hedihamida98@gmail.com>
2022-06-16 10:13:17 +0100

    new

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,5 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
-	char *ptr = dest + strlen(dest);
-
-	while (*src != '\0' && --n)
-	{
-		*dest++ = *src++;
-	}
-
-	*ptr = '\0';
-
-	return (dest);
+	strncat(dest, src, n);
 }
