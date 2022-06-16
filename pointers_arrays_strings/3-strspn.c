0a078812513c8451f5df8377b30d54adf78458ef
hedi <hedihamida98@gmail.com>
2022-06-16 14:06:18 +0100

    new

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,21 @@
+
+#include "main.h"
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; *(s + i) != '\0'; ++i)
+		;
+
+	return (i);
+}
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 	int k, j;
 
-	for (j = 0; *s != '\0'; j++)
+	for (j = 0; j <= _strlen(s); j++)
 	{
-		for (k = 0; *s != '\0'; k++)
+		for (k = 0; k <= _strlen(accept); k++)
 		{
 			if (s[j] == *(accept + k))
 			{
