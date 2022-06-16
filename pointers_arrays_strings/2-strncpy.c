baa5123d59a66a17f5fa46b424123ca3e303a41c
hedi <hedihamida98@gmail.com>
2022-06-16 11:05:27 +0100

    new

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-
+	int i;
 	char *ptr = dest;
 	while (*src != '\0' && n--)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+		i++;
 	}
 
-	*dest = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
-	return (ptr);
+		return (ptr);
 }
