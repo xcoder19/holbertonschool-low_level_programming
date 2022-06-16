5942fdae046420abcdf3813cc05fd8df545db340
hedi <hedihamida98@gmail.com>
2022-06-16 13:16:32 +0100

    new

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+ * @brief
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	return NULL;
+}
