528b613ea8519f3d1cb7e47b8b79a46ff3be77cd
hedi <hedihamida98@gmail.com>
2022-06-18 18:37:14 +0100

    new

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,26 @@
 #include "main.h"
 #include <string.h>
+
+/**
+ * _strlen - counting
+ * @s: pointer to the first char of a string
+ * Return: length of string
+ */
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; *(s + i) != '\0'; ++i)
 		;
 
 	return (i);
 }
+/**
+ * _strstr - strstr
+ * @haystack: char pointer
+ * @needle: char pointer
+ * Return: char pointer to the first occurence
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
@@ -18,7 +31,7 @@ char *_strstr(char *haystack, char *needle)
 	j = 0;
 	if (*needle == '\0')
 	{
-		return haystack;
+		return (haystack);
 	}
 	while (haystack[i] != '\0' && needle[j] != '\0')
 	{
@@ -41,8 +54,7 @@ char *_strstr(char *haystack, char *needle)
 	if (j == len)
 	{
 		return (arr - j);
-		putchar('\n');
 	}
 
-	return NULL;
+	return (NULL);
 }
