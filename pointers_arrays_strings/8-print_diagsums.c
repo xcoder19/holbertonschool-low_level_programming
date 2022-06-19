fae34197efbff675d3dc2ea1243d51c9fec0b688
hedi <hedihamida98@gmail.com>
2022-06-19 18:42:24 +0100

    new

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -5,4 +5,19 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i, val1 = 0, val2 = 0;
+
+	for (i = 0; i < size * size; i += size + 1)
+	{
+		val1 += a[i];
+	}
+
+	for (i = size - 1; i <= (size * (size - 1)); i += (size - 1))
+	{
+		val2 += a[i];
+	}
+
+	printf("%d\n", val1);
+
+	printf("%d\n", val2);
 }
