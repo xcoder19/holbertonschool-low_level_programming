b7b95c45a339e83035622ee8557ef1e661443dd8
hedi <hedihamida98@gmail.com>
2022-06-21 10:51:57 +0100

    new

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+
+
+#include "main.h"
+
+/**
+ *_pow_recursion - power
+ * @x: int
+ * @y: int
+ * Return: x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
