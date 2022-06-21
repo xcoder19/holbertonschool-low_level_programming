eb8ae87b75b80cc4b7ce66fbdae85f8f58473702
hedi <hedihamida98@gmail.com>
2022-06-21 11:21:31 +0100

    new

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,6 +5,27 @@
  * Return: 1 or 0
  */
 
+int isprime(int i, int j)
+{
+	if (j < 2 || j % i == 0)
+	{
+		return (0);
+	}
+
+	if (i > (j / 2))
+	{
+		return (1);
+	}
+
+	return (isprime(i + 1, j));
+}
+
 int is_prime_number(int n)
 {
+	if (n == 2)
+	{
+		return (1);
+	}
+
+	return (isprime(2, n));
 }
