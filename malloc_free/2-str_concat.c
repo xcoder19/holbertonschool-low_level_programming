691c3fc439c542608d129bab1697d85e665d5198
hedi <hedihamida98@gmail.com>
2022-06-23 22:58:59 +0100

    new

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,7 +3,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr, *k;
-
+	if (s1 == NULL)
+	{
+		*s1 == "";
+	}
+	if (s2 == NULL)
+	{
+		*s2 == "";
+	}
 	arr = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 
 	while (arr != NULL)
