9b630c23541a741631e3801560353849ab4cab56
hedi <hedihamida98@gmail.com>
2022-06-23 13:39:57 +0100

    new

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-
-	if (arr = (char *)malloc(size * sizeof(char)) != NULL)
+	arr = (char *)malloc(size * sizeof(char));
+	if (arr != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
