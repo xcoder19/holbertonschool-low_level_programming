af872272ac49c53fa9b23f09a7a09faaf702a41c
hedi <hedihamida98@gmail.com>
2022-06-23 09:57:59 +0100

    new

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - outputs the name of the file
+ * @argc: counts of arguments
+ * @argv: array of values
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+
+	putchar('0' + argc);
+	putchar('\n');
+	return (0);
+}
