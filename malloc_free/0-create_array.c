67005056d8f8f802142cc86303ec3bade3fb1420
hedi <hedihamida98@gmail.com>
2022-06-23 13:23:29 +0100

    new

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+ * create_array - create array dynamicaly
+ * @size: int
+ * @char: character
+ * Return: arr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+	arr = (char *)malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return arr;
+}
