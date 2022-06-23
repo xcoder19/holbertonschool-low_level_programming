3328fe59c681846c52e209655d783398f79dd925
hedi <hedihamida98@gmail.com>
2022-06-23 09:56:14 +0100

    new

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - outputs the name of the file
+ * @argc: counts of arguments
+ * @argv: array of values
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
 
