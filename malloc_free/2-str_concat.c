fcb0b8f943a57fb2c53feca4cf1c966ec97d7c5c
hedi <hedihamida98@gmail.com>
2022-06-23 22:44:29 +0100

    new

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,21 +2,25 @@
 #include "main.h"
 char *str_concat(char *s1, char *s2)
 {
-	char *arr;
-	char *p;
+	char *arr, *k;
 
-	arr = (char *)malloc(sizeof(char) * strlen(s1) + 1);
-	p = arr;
-	while (arr != NULL)
-	{
+	int i;
 
-		*arr++ = *s1++;
-	}
+	arr = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 
 	while (arr != NULL)
 	{
-		*arr++ = *s2++;
+		char *k = s1 + strlen(s1);
+
+		while (*s2 != '\0')
+		{
+			*k++ = *s2++;
+		}
+
+		*k = '\0';
 	}
 
-	return p;
+	arr = k;
+
+	return arr;
 }
