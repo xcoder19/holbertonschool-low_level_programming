4006e982fc2811716b7c669523e5dea60cdcb68e
hedi <hedihamida98@gmail.com>
2022-06-24 22:02:39 +0100

    new

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	while (s1 != '\0')
+	{
+		if (*s1++ != *s2++)
+		{
+			if (*s1 > *s2)
+			{
+				return (1);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+	}
+
+	return (0);
+}
