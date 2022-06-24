c01c8d3d6ada7181abc5d1cb37685e632d8f52b5
hedi <hedihamida98@gmail.com>
2022-06-24 21:54:55 +0100

    new

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,12 +1,19 @@
 
 #include "main.h"
+/**
+ * free_grid - free the previous 2d array
+ * @grid: 2d array
+ * @height: rows of array
 
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
+
 	free(grid);
 }
