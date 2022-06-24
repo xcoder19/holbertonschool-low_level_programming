0f0ab9657d4905291076c9039df8008fd8578674
hedi <hedihamida98@gmail.com>
2022-06-24 21:31:21 +0100

    new

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,14 +25,14 @@ int **alloc_grid(int width, int height)
 
 	else
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(p[i]);
-		}
-		free(p);
 
 		return (NULL);
 	}
 
 	return (p);
+	for (i = 0; i < height; i++)
+	{
+		free(p[i]);
+	}
+	free(p);
 }
