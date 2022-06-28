1cb0891d65e90e5e3ba815c6a5b480fc1f7fa3a7
hedi <hedihamida98@gmail.com>
2022-06-28 20:21:51 +0100

    new

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,11 +11,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	char *p;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 	{
 		*s1 = "";
 	}
-	if (*s2 == NULL)
+	if (s2 == NULL)
 	{
 		*s2 = "";
 	}
