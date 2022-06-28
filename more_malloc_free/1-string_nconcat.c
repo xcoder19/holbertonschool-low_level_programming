505297008607c30b6e070bacd6f8824223b6d617
hedi <hedihamida98@gmail.com>
2022-06-28 21:39:50 +0100

    new

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	int i, j;
-	unsigned int len;
+	unsigned int len, m, k;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -23,16 +23,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	if (n >= (strlen(s2)))
+	for (m = 0; s1[m] != '\0'; m++)
+		;
+	for (k = 0; s2[k] != '\0'; k++)
+		;
+	if (n >= k)
 	{
-		len = strlen(s1) + strlen(s2);
+		len = k + m;
 	}
 	else
 	{
-		len = strlen(s1) + n;
+		len = m + n;
 	}
 
-	p = (char *)malloc(sizeof(char) * len + 1);
+	p = (char *)malloc(len + 1);
 	if (p != NULL)
 	{
 
