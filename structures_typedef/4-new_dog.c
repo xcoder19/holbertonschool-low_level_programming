2b0b19bb8460bfe72ab989ed02823f58ce1207b1
hedi <hedihamida98@gmail.com>
2022-06-30 21:27:23 +0100

    new

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,22 +7,21 @@
  * @owner: owner
  * Return: return a pointer to dynamicly allocated struct
  */
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new_dogi;
+	char *o, *n;
+	new_dogi = malloc(sizeof(dog_t));
 
-	char *n, *o;
-
-	new_dog = malloc(sizeof(dog_t));
-
-	if (new_dog != NULL)
+	if (new_dogi != NULL)
 	{
 
 		n = malloc(sizeof(name));
 		if (n == NULL)
 		{
 			free(n);
-			free(new_dog);
+			free(new_dogi);
 			return (NULL);
 		}
 		o = malloc(sizeof(owner));
@@ -30,18 +29,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 		{
 			free(n);
 			free(o);
-			free(new_dog);
+			free(new_dogi);
 			return (NULL);
 		}
 
-		n = strcpy(new_dog->name, name);
-		o = strcpy(new_dog->owner, owner);
-		new_dog->age = age;
-		return (new_dog);
+		n = _strcpy(new_dogi->name, name);
+		o = _strcpy(new_dogi->owner, owner);
+		new_dogi->age = age;
+		return (new_dogi);
 	}
 	else
 	{
-		free(new_dog);
+		free(new_dogi);
 		return (NULL);
 	}
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
 }
