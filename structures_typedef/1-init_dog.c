8fceebee07408928b5252a074958432c1abd9da4
hedi <hedihamida98@gmail.com>
2022-06-30 16:37:56 +0100

    new

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+
+#include "dog.h"
+/**
+ * init_dog - intializes dog struct
+ * @d: struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	*d->name = name;
+	*d->age = age;
+	*d->owner = owner;
+}
