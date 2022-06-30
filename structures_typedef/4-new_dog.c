fa8608b07eca66757436fa02b48118b910a64a7b
hedi <hedihamida98@gmail.com>
2022-06-30 17:20:33 +0100

    new

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,7 +2,11 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog = {name, age, owner};
+	dog_t dog;
+
+	dog.name = name;
+	dog.age = age;
+	dog.owner = owner;
 
 	return &dog;
 }
