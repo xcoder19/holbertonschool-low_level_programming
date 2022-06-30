138bf21792f946f788093df5146b978c48f4a488
hedi <hedihamida98@gmail.com>
2022-06-30 20:53:03 +0100

    new

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,30 @@
 #include "dog.h"
-
+/**
+ * new_dog - dynamicly allocats a struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: a
+ * @return:return a pointer to dynamicly allocated struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	if (name && age && owner)
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog != NULL)
 	{
-		return dog;
+		name = malloc(strlen(name) + 1);
+		owner = malloc(strlen(owner) + 1);
+
+		new_dog->name = name;
+		new_dog->owner = owner;
+		new_dog->age = age;
+		return new_dog;
 	}
-	return dog;
+	else
+	{
+		return (NULL);
+	}
 }
