58f7bf09152ef9389104ff99be0f2fff6d11d5a4
hedi <hedihamida98@gmail.com>
2022-07-01 22:10:30 +0100

    new

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+
+
+#include "3-calc.h"
+/**
+ * op_add - adds two ints
+ * @a:int
+ * @b:int
+ * Return: result
+ */
+int op_add(int a, int b) { return a + b; }
+/**
+ * op_sub - sub two ints
+ * @a:int
+ * @b:int
+ * Return: result
+ */
+int op_sub(int a, int b) { return a - b; }
+/**
+ * op_mul - multiply two ints
+ * @a:int
+ * @b:int
+ * Return: result
+ */
+int op_mul(int a, int b) { return a * b; }
+/**
+ * opp_div - divide a by b
+ * @a:int
+ * @b:int
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return a / b;
+}
+/**
+ * op_mod - mod a by b
+ * @a:int
+ * @b:int
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return a % b;
+}
