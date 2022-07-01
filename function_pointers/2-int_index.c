47760b9385fec7a3af83db24a77da0bb5167509e
hedi <hedihamida98@gmail.com>
2022-07-01 21:18:14 +0100

    new

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+
+#include "function_pointers.h"
+/**
+ * int_index - searches for an int
+ * @array: array of int
+ * @size: size of array
+ * @cmp: pointer to a function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (size)
+	{
+		if ((*cmp)(array[i]))
+		{
+			return array[i];
+		}
+		i++;
+		size--;
+	}
+}
