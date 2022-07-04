7705dac9bd9fd27414b496ae96814cd51f29407b
hedi <hedihamida98@gmail.com>
2022-07-04 20:51:51 +0100

    new

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL)
 		{
 
-			printf("%s%s", va_arg(ap, char *), separator);
+			printf("%s%s", separator, va_arg(ap, char *));
 		}
 		else
 		{
