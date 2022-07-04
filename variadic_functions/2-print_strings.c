4a070bf6a8d646288a2aaac8109a28b5d6e3fea7
hedi <hedihamida98@gmail.com>
2022-07-04 21:10:29 +0100

    new

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,4 +1,9 @@
 #include "variadic_functions.h"
+/**
+ * print_strings - print string args
+ * @separator: separator
+ * @n: number of args
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
@@ -6,21 +11,38 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL)
 		{
-
-			printf("%s", va_arg(ap, char *));
-			if (i < n - 1)
+			if (va_arg(ap, char *) != NULL)
 			{
-				printf("%s", separator);
+				printf("%s", va_arg(ap, char *));
+				if (i < n - 1)
+				{
+					printf("%s", separator);
+				}
+			}
+			else
+			{
+				printf("%s", "(nil)");
+				if (i < n - 1)
+				{
+					printf("%s", separator);
+				}
 			}
 		}
 		else
 		{
-
-			printf("%s", va_arg(ap, char *));
+			if (va_arg(ap, char *) != NULL)
+			{
+				printf("%s", va_arg(ap, char *));
+			}
+			else
+			{
+				printf("%s", "(nil)");
+			}
 		}
 	}
 	putchar('\n');
