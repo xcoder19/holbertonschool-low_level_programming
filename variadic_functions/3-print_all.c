bb99d27ae49e885adf65411dedb5a1e670551212
hedi <hedihamida98@gmail.com>
2022-07-04 23:31:18 +0100

    new

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,12 +1,64 @@
 #include "variadic_functions.h"
+
+/**
+ * print_char - print char
+ * @ap: list
+ */
+
+void print_char(va_list *ap)
+{
+	char c;
+
+	c = va_arg(*ap, int);
+	printf("%c", c);
+}
+/**
+ * print_int - print int
+ * @ap:list
+ */
+void print_int(va_list *ap)
+{
+	int i;
+
+	i = va_arg(*ap, int);
+	printf("%d", i);
+}
+/**
+ * print_float - print float
+ * @ap: list
+ */
+void print_float(va_list *ap)
+{
+	float f;
+
+	f = va_arg(*ap, double);
+	printf("%f", f);
+}
+/**
+ * print_str - print str
+ * @ap: list
+ */
+void print_str(va_list *ap)
+{
+	char *str;
+
+	str = va_arg(*ap, char *);
+
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+
+	printf("%s", str);
+}
 /**
  * print_all - print anything
  * @format: format
  */
 void print_all(const char *const format, ...)
 {
-	char *str, c;
-	int k, m;
+
+	int m;
 	float j;
 
 	unsigned long i = 0;
@@ -21,29 +73,20 @@ void print_all(const char *const format, ...)
 		{
 
 		case 'c':
-			c = va_arg(ap, int);
-
-			printf("%c", c);
+			print_char(&ap);
 
 			break;
 		case 'f':
+
 			j = va_arg(ap, double);
 			printf("%f", j);
 			break;
 		case 's':
+			print_str(&ap);
 
-			str = va_arg(ap, char *);
-			if (str != NULL)
-			{
-				printf("%s", str);
-				break;
-			}
-
-			printf("%s", "(nil)");
 			break;
 		case 'i':
-			k = va_arg(ap, int);
-			printf("%d", k);
+			print_int(&ap);
 			break;
 
 		default:
