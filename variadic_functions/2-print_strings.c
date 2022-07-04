cb71e04364b716e164a7a12486bcc2ea20f95de0
hedi <hedihamida98@gmail.com>
2022-07-04 20:49:57 +0100

    new

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,25 +10,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		if (separator != NULL)
 		{
-			if (va_arg(ap, char *) == NULL)
-			{
-				printf("%s%s", "(nil)", separator);
-			}
-			else
-			{
-				printf("%s%s", va_arg(ap, char *), separator);
-			}
+
+			printf("%s%s", va_arg(ap, char *), separator);
 		}
 		else
 		{
-			if (va_arg(ap, char *) == NULL)
-			{
-				printf("%s%s", "(nil)", separator);
-			}
-			else
-			{
-				printf("%s", va_arg(ap, char *));
-			}
+
+			printf("%s", va_arg(ap, char *));
 		}
 	}
 	va_end(ap);
