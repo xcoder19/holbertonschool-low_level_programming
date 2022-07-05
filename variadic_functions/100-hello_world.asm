68e5b6b5df185cc4396167448ea737f6d24031cc
hedi <hedihamida98@gmail.com>
2022-07-05 01:13:58 +0100

    new

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,15 @@
-section .data
-    msg db "Hello world!",10 
-
-section .text
-    global _start
-
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+section .data 
+    text db "Hello, world!", 10 
+     
+section .text 
+global _start 
+_start: 
+    mov rax, 1 
+    mov rdi, 1 
+    mov rsi, text 
+    mov rdx, 14 
+    syscall 
+     
+    mov rax, 60 
+    mov rdi, 0 
+    syscall 
