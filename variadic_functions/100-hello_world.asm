8f39245edf680378267b1c033930679c6d280339
hedi <hedihamida98@gmail.com>
2022-07-05 01:27:05 +0100

    new

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,14 @@
-    	SECTION .data
-    msg: db "Hello, World"
-    msg: db 10
-    	SECTION .text 
-    	global main 
-    main: 
-    	mov edx, 12 
-    	mov ecx, msg 
-    	mov ebx, 1 
-    	mov eax, 4 
-    	int 0x80 
+SECTION .data
+    msg: db "Hello, World",13
+SECTION .text 
+global main 
+main: 
+    mov edx, 12 
+    mov ecx, msg 
+    mov ebx, 1 
+    mov eax, 4 
+    int 0x80 
      
-    	mov ebx, 0 
-    	mov eax, 1 
-    	int 0x80 
+    mov ebx, 0 
+    mov eax, 1 
+    int 0x80 
