7c5bf269573ab394fdeec527fc828e965289e7c0
hedi <hedihamida98@gmail.com>
2022-07-14 20:48:23 +0100

    new

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * list_len - list number of elements
+ * @h: pointer to a linked list
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	int len = 0;
