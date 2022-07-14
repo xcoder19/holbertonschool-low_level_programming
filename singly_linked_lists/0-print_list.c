7916017d5a766b76072620b7189a1b98477542b0
hedi <hedihamida98@gmail.com>
2022-07-14 12:00:44 +0100

    new

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,7 +5,15 @@ size_t print_list(const list_t *h)
 	int len = 0;
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
 		h = h->next;
 		len++;
 	}
