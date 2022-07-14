fa32c64796dbfb3b53e451f1dcccd1a038b8ee71
hedi <hedihamida98@gmail.com>
2022-07-14 21:14:10 +0100

    new

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *node = *head;
+	node = malloc(sizeof(list_t));
+
+	node->str = strdup(str);
+	node->len = strlen(str);
+	node->next = NULL;
+
+	return *head;
+}
