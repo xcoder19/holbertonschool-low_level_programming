ddb6dd6ede207e868729b352ca52d0f8a20ae1e9
hedi <hedihamida98@gmail.com>
2022-07-17 18:00:57 +0100

    new

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,14 @@
 #include "lists.h"
-
+/**
+ * free_listint - frees a list
+ * @head: pointer to head
+ */
 void free_listint(listint_t *head)
 {
+	if (head == NULL)
+	{
+		return;
+	}
 	while (head->next != NULL)
 	{
 
