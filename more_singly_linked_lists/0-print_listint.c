3a84cbbb6f3f174e67f9b94264cab69fb25f4e4b
hedi <hedihamida98@gmail.com>
2022-07-17 10:55:24 +0100

    new

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,19 @@
 
 #include "lists.h"
+/**
+ * print_listint - prints all the elements of a list
+ * @h: header of a list
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t len = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		len++;
 	}
-	return len;
+	return (len);
 }
