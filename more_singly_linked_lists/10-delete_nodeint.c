cde26553285528e89dcf8527518d799611edd039
hedi <hedihamida98@gmail.com>
2022-07-18 18:02:25 +0100

    new

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,31 @@
+
+#include "lists.h"
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *p = *head;
+	if (*head == NULL)
+	{
+		return -1;
+	}
+	if (index == 0)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	while (index != 0)
+	{
+
+		if (index == 1)
+		{
+			tmp = p->next;
+			p->next = p->next->next;
+			free(tmp);
+			break;
+		}
+		p = p->next;
+		index--;
+	}
+
+	return 1;
+}
