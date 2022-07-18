3282966a631bb4517ad7c38a027ad9b0a93d7df0
hedi <hedihamida98@gmail.com>
2022-07-18 11:20:32 +0100

    new

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
 	while (*head != NULL)
 	{
-		*head = (*head)->next;
+		tmp = (*head)->next;
 		free(*head);
+		*head = tmp;
 	}
-	free(*head);
 }
