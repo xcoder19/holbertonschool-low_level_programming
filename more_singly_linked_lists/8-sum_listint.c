488b3dc18f40d9dfb83fda4ac6c4b412e019f0d5
hedi <hedihamida98@gmail.com>
2022-07-18 14:03:36 +0100

    new

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,13 @@
 #include "lists.h"
+/**
+ * sum_listint - sum of all the data of a linked list
+ * @head: pointer to head of a list
+ * Return: sum
+ */
 int sum_listint(listint_t *head)
 {
 	int i = 0;
+
 	if (head == NULL)
 	{
 		return (0);
