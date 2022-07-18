89f41a55177ecd1cca414eaf2eedb3956026eb59
hedi <hedihamida98@gmail.com>
2022-07-18 17:29:56 +0100

    new

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			if (idx == 1)
 			{
-				if (p->next = NULL)
+				if (p->next == NULL)
 				{
 
 					p->next = node;
