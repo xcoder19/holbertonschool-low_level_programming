97984c3717eeec5574e9caff362310634a564f36
hedi <hedihamida98@gmail.com>
2022-07-18 17:15:45 +0100

    new

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ void free_listint(listint_t *head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int _strlen(const char *s);
 #endif
