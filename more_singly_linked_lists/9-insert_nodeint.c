9f2fb7de2a10bc5836eb31a9d8272942f02869a2
hedi <hedihamida98@gmail.com>
2022-07-18 17:20:16 +0100

    new

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			node->next = (*head)->next;
 			(*head) = node;
-			return (*head);
+			return (node);
 		}
 		while (p != NULL)
 		{
