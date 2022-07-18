f5253a7fec958e1705859bf027af4e5fc07a260b
hedi <hedihamida98@gmail.com>
2022-07-18 11:23:43 +0100

    new

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,12 @@
 #include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: pointer to listint_t pointer
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+
 	if (head == NULL)
 	{
 		return;
