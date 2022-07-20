1208971db25bbc9e991b99e40908c713161f64c4
hedi <hedihamida98@gmail.com>
2022-07-20 18:39:45 +0100

    new

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		k = read(n, buffer, letters);
 
-		write(2, buffer, k);
+		write(STDOUT_FILENO, buffer, k);
 		close(n);
 	}
 	return k;
