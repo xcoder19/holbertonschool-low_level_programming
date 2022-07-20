771d62ba7a7c12ace47242db585d677b78ab0bfa
hedi <hedihamida98@gmail.com>
2022-07-20 12:21:31 +0100

    new

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,13 +1,20 @@
 #include "main.h"
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: decimal
+ * @index: index
+ * Return: 1 or 0 on success
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int m, bt = 0;
 	int i;
 	unsigned long k;
+
 	if (n == 0)
 	{
 		printf("0");
-		return 0;
+		return (0);
 	}
 	for (i = 64; i >= 0; i--)
 	{
@@ -23,8 +30,8 @@ int get_bit(unsigned long int n, unsigned int index)
 		m = i;
 		if (m == index)
 		{
-			return bt;
+			return (bt);
 		}
 	}
-	return -1;
+	return (-1);
 }
