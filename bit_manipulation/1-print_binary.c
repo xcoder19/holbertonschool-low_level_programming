bac8f5fcc4a93bce37b997a91a604b870892c5b7
hedi <hedihamida98@gmail.com>
2022-07-20 11:57:57 +0100

    new

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+void print_binary(unsigned long int n)
+{
+	int i, flag = 0;
+	unsigned long k;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	for (i = 16; i >= 0; i--)
+	{
+		k = n >> i;
+		if (k & 1)
+		{
+			printf("1");
+			flag = 1;
+		}
+		else if (flag)
+		{
+			printf("0");
+		}
+	}
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,4 +4,5 @@
 unsigned int binary_to_uint(const char *b);
 int _pow_recursion(int x, int y);
 int _strlen(const char *s);
+void print_binary(unsigned long int n);
 #endif
