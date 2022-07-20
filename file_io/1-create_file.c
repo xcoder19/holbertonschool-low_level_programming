7a5cfff9f3ca2f3edc89c140fd01d7f7c1d99da4
hedi <hedihamida98@gmail.com>
2022-07-20 21:30:49 +0100

    new

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,11 +1,17 @@
 #include "main.h"
+/**
+ * create_file - create file
+ * @filename: filename
+ * @text_content: string to write
+ * Return: 1 on success
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int n;
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	n = open(filename, O_RDWR | O_CREAT);
@@ -15,13 +21,13 @@ int create_file(const char *filename, char *text_content)
 		{
 			write(n, "", 0);
 			close(n);
-			return 1;
+			return (1);
 		}
 		write(n, text_content, strlen(text_content));
 		close(n);
-		return 1;
+		return (1);
 	}
 
 	close(n);
-	return -1;
+	return (-1);
 }
