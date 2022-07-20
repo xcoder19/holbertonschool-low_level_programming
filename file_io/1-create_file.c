95864bf2574402362a8244b73825e1f8700638b7
hedi <hedihamida98@gmail.com>
2022-07-20 21:28:46 +0100

    new

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,9 +11,15 @@ int create_file(const char *filename, char *text_content)
 	n = open(filename, O_RDWR | O_CREAT);
 	if (n != -1)
 	{
+		if (text_content == NULL)
+		{
+			write(n, "", 0);
+			close(n);
+			return 1;
+		}
 		write(n, text_content, strlen(text_content));
 		close(n);
-		return -1;
+		return 1;
 	}
 
 	close(n);
