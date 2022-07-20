9a20dd51f5eb1e3ee8264e86d29f034efa8a029d
hedi <hedihamida98@gmail.com>
2022-07-20 23:23:59 +0100

    new

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	n = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	n = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 	if (n != -1)
 	{
 
