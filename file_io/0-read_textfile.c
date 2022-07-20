65cd5ead9221105894d44947981f93d2bdc820bc
hedi <hedihamida98@gmail.com>
2022-07-20 17:53:14 +0100

    new

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,7 +5,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int n;
 	char *buffer;
 	buffer = malloc(letters * sizeof(char));
-	n = open(filename, O_RDWR);
+	n = open(filename, O_RDONLY);
 	if (n != -1)
 	{
 		read(n, buffer, letters);
