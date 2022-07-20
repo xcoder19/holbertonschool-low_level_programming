e9a7c6be1405d7a3138f0f2c2d1b8ef8e85ac4a2
hedi <hedihamida98@gmail.com>
2022-07-20 12:13:55 +0100

    new

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i, bt = 0;
+
+	unsigned long k;
+	if (n == 0)
+	{
+		printf("0");
+		return 0;
+	}
+	for (i = 63; i >= 0; i--)
+	{
+		k = n >> i;
+		if (k & 1)
+		{
+			bt = 1;
+		}
+		else
+		{
+			bt = 0;
+		}
+
+		if (i == index)
+		{
+			return bt;
+		}
+	}
+	return 0;
+}
