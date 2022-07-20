4855c5c79e893d8b5c0676f4c19223b5db64172f
hedi <hedihamida98@gmail.com>
2022-07-20 21:27:07 +0100

    new

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 int create_file(const char *filename, char *text_content)
 {
+	int n;
+
 	if (filename == NULL)
 	{
 		return -1;
 	}
-
-	int n;
 
 	n = open(filename, O_RDWR | O_CREAT);
 	if (n != -1)
