1b0dbe240663f87424d8cce2c1ccf4e8ac0fab5b
hedi <hedihamida98@gmail.com>
2022-07-20 18:47:56 +0100

    new

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,10 +1,17 @@
 #include "main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
+/**
+ * read_textfile - read content of a file and outputs it
+ * @filename: path of file
+ * @letters: number of letters
+ * Return: number of letters printed
+ */
 
 {
 	int n;
 	size_t k;
 	char *buffer;
+
 	buffer = malloc(letters * sizeof(char));
 	n = open(filename, O_RDONLY);
 	if (n != -1)
@@ -14,5 +21,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		write(STDOUT_FILENO, buffer, k);
 		close(n);
 	}
-	return k;
+	return (k);
 }
