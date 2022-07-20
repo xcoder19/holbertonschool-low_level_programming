fd9e8bcbd054e4aeed03a8637bef89247dd07476
hedi <hedihamida98@gmail.com>
2022-07-20 17:46:54 +0100

    new

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+	int n;
+	char buffer[letters];
+	n = open(filename, O_RDWR);
+	if (n != -1)
+	{
+		read(n, buffer, sizeof(buffer));
+		write(1, buffer, sizeof(buffer));
+		close(n);
+	}
+	return letters;
+}
---
+++ file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
