1761dfe6d747f12e01ae88705ff45ab304a864f4
hedi <hedihamida98@gmail.com>
2022-07-20 11:19:24 +0100

    new

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,63 @@
+
+#include "main.h"
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: binary string
+ * Return: decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, dec = 0, temp, m = _strlen(b) - 1;
+
+	i = 0;
+
+	while (b[m - i])
+	{
+
+		if (b[m - i] != 48 && b[m - i] != 49)
+		{
+			return 0;
+		}
+
+		temp = b[m - i] - '0';
+
+		dec = dec + (temp * _pow_recursion(2, i));
+		i++;
+	}
+	return dec;
+}
+
+/**
+ *_pow_recursion - power
+ * @x: int
+ * @y: int
+ * Return: x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * _strlen - counting
+ * @s: pointer to the first char of a string
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int i;
+	for (i = 0; *(s + i) != '\0'; ++i)
+		;
+
+	return (i);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _lists_h
+#define _lists_h
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b);
+int _pow_recursion(int x, int y);
+int _strlen(const char *s);
+#endif
