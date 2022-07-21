b4a6eb38a74d9c050b2501b8595a4b45349c0c24
hedi <hedihamida98@gmail.com>
2022-07-21 23:28:46 +0100

    new

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,10 +11,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		node->next = (*head);
 		node->prev = NULL;
 		(*head) = node;
-		if (*head != NULL)
-		{
-			(*head)->prev = node;
-		}
 
 		return (*head);
 	}
