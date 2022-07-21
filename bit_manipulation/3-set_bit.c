92435b3197c8a0067e0cb6fe05b6c460cf4681c7
hedi <hedihamida98@gmail.com>
2022-07-21 22:13:21 +0100

    new

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -5,5 +5,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int clearBit = ~(1 << index);
 	unsigned long int mask = *n & clearBit;
 	*n = (mask | (1 << index));
-	return *n;
+	return 1;
 }
