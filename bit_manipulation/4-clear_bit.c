f1ba4724143bca5fc4c52defde72aec48ef19207
hedi <hedihamida98@gmail.com>
2022-07-21 22:36:33 +0100

    new

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int clearBit = ~(0 << index);
+	unsigned long int clearBit = ~(1 << index);
 	unsigned long int mask = *n & clearBit;
-	*n = (mask | (1 << index));
+	*n = (mask | (0 << index));
 	return (1);
 }
