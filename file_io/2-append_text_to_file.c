d7797031be8e0c84b141e2c96c476ecb7fd65519
hedi <hedihamida98@gmail.com>
2022-07-21 08:57:17 +0100

    new

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,6 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd != -1)
 	{
+		if (text_content == NULL)
+		{
+			write(fd, "", strlen(""));
+			return 1;
+		}
 
 		write(fd, text_content, strlen(text_content));
 
