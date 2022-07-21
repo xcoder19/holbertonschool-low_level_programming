dbb862f50de389f5f4853cd8d5ef0ba8ed254df5
hedi <hedihamida98@gmail.com>
2022-07-21 22:32:57 +0100

    new

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,9 +1,14 @@
 #include "main.h"
-
+/**
+ * set_bit - set the value of a bit to 1
+ * @n: pointer to int
+ * @index: index
+ * Return: 1 on success
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int clearBit = ~(1 << index);
 	unsigned long int mask = *n & clearBit;
 	*n = (mask | (1 << index));
-	return 1;
+	return (1);
 }
