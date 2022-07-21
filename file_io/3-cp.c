e8bce57fc97a934def5e3d07847d4662be99a901
hedi <hedihamida98@gmail.com>
2022-07-21 09:44:16 +0100

    new

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,29 +8,32 @@ int main(int argc, char *argv[])
 	buffer = malloc(1024);
 	if (argc != 3)
 	{
-		exit(97);
+
 		write(STDERR_FILENO, "\n", 1);
+		exit(97);
 	}
 	fd = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 00664);
 	fd2 = open(argv[1], O_RDONLY);
 
 	if (fd2 == -1)
 	{
-		exit(98);
+
 		err = "Error: Can't read from file ";
 		write(STDERR_FILENO, err, strlen(err));
 		write(STDERR_FILENO, argv[1], strlen(argv[1]));
 		write(STDERR_FILENO, "\n", 1);
 		free(err);
+		exit(98);
 	}
 	if (fd == -1)
 	{
-		exit(99);
+
 		err = "Error: Can't write to ";
 		write(STDERR_FILENO, err, strlen(err));
 		write(STDERR_FILENO, argv[2], strlen(argv[2]));
 		write(STDERR_FILENO, "\n", 1);
 		free(err);
+		exit(99);
 	}
 	read(fd2, buffer, 1024);
 	dprintf(fd, "%s", buffer);
@@ -40,11 +43,10 @@ int main(int argc, char *argv[])
 	{
 		err = "Error: Can't close fd FD_VALUE ";
 		write(STDERR_FILENO, err, strlen(err));
-		exit(100);
-
 		write(STDERR_FILENO, "-1", strlen("-1"));
 		write(STDERR_FILENO, "\n", 1);
 		free(err);
+		exit(100);
 	}
 
 	free(err);
