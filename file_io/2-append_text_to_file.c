1cb8ebfebd744084dc9e3c9ef15f4dbbc3b4e64a
hedi <hedihamida98@gmail.com>
2022-07-21 08:54:24 +0100

    new

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - apend text at the end of a file
+ * @filename: filename
+ * @text_content: text 
+ * Return: 1 on success
+ * /
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	if (filename == NULL)
+	{
+		return -1;
+	}
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd != -1)
+	{
+
+		write(fd, text_content, strlen(text_content));
+
+		close(fd);
+		return (1);
+	}
+	close(fd);
+	return (-1);
+}
