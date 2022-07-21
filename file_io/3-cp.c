56cdba8f108cc93dd2f4d81a20c86232a6ada6f4
hedi <hedihamida98@gmail.com>
2022-07-21 16:57:38 +0100

    new

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,70 +1,49 @@
 #include "main.h"
+void throwerr(char *err, char *filename, int errcode)
+{
+	if (filename == NULL)
+	{
+		dprintf(STDERR_FILENO, "%s\n", err);
+		exit(errcode);
+	}
+	dprintf(STDERR_FILENO, "%s%s\n", err, filename);
+	exit(errcode);
+}
 int main(int argc, char *argv[])
 {
 	int fd, fd2, fd3, n, k, fd4;
-	char *err, *buffer;
+	char *buffer;
 
-	err = malloc(32);
 	buffer = malloc(1024);
 	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to", strlen("Usage: cp file_from file_to"));
-		write(STDERR_FILENO, "\n", 1);
-		exit(97);
-	}
+		throwerr("Usage: cp file_from file_to", NULL, 97);
+
 	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 00664);
 	fd2 = open(argv[1], O_RDONLY);
 	if (fd2 == -1)
-	{
-		err = "Error: Can't read from file ";
-		write(STDERR_FILENO, err, strlen(err));
-		write(STDERR_FILENO, argv[1], strlen(argv[1]));
-		write(STDERR_FILENO, "\n", 1);
-		exit(98);
-	}
+		throwerr("Error: Can't read from file ", argv[1], 98);
+
 	if (fd == -1)
-	{
-		err = "Error: Can't write to ";
-		write(STDERR_FILENO, err, strlen(err));
-		write(STDERR_FILENO, argv[2], strlen(argv[2]));
-		write(STDERR_FILENO, "\n", 1);
-		exit(99);
-	}
+		throwerr("Error: Can't write to ", argv[2], 99);
+
 	fd3 = read(fd2, buffer, 1024);
 	if (fd3 == -1)
-	{
-		err = "Error: Can't read from file ";
-		write(STDERR_FILENO, err, strlen(err));
-		write(STDERR_FILENO, argv[1], strlen(argv[1]));
-		write(STDERR_FILENO, "\n", 1);
-		exit(98);
-	}
+		throwerr("Error: Can't read from file ", argv[1], 98);
+
 	while (fd3 > 0)
 	{
 		fd4 = write(fd, buffer, fd3);
 		if (fd4 == -1)
-		{
-			err = "Error: Can't write to ";
+			throwerr("Error: Can't write to ", argv[2], 99);
 
-			dprintf(2, "%s%s\n", err, argv[2]);
-
-			exit(99);
-		}
 		fd3 = read(fd2, buffer, 1024);
 		if (fd3 < 0)
 			break;
 	}
-
 	n = close(fd);
 	k = close(fd2);
 	if (n == -1 || k == -1)
-	{
-		err = "Error: Can't close fd ";
-		write(STDERR_FILENO, err, strlen(err));
-		write(STDERR_FILENO, "-1", strlen("-1"));
-		write(STDERR_FILENO, "\n", 1);
-		exit(100);
-	}
+		throwerr("Error: Can't close fd -1", NULL, 100);
 
 	return (0);
 }
