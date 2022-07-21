03d9474b262079d08d76fa66a4032c0f105cae15
hedi <hedihamida98@gmail.com>
2022-07-21 08:55:28 +0100

    new

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * append_text_to_file - apend text at the end of a file
  * @filename: filename
- * @text_content: text 
+ * @text_content: text
  * Return: 1 on success
- * /
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
