ea9db43fcce2be54fdc31114a8940a0dbe851c0c
hedi <hedihamida98@gmail.com>
2022-07-21 22:38:01 +0100

    new

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,3 +1,9 @@
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: pointer to int
+ * @index: index
+ * Return: 1 on success
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int clearBit = ~(1 << index);
