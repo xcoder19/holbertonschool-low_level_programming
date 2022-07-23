658eff4a0e8df49e0783b2b3cf2fba9276a71d16
hedi <hedihamida98@gmail.com>
2022-07-23 13:10:34 +0100

    new

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_dlistint - free list
- * @head: pointer to doubly linked list
+ * @head: pointer to dlistint_t
  */
 void free_dlistint(dlistint_t *head)
 {
@@ -9,9 +9,10 @@ void free_dlistint(dlistint_t *head)
 	{
 		return;
 	}
-	while (head != NULL)
+	while (head->next != NULL)
 	{
 		free(head);
 		head = head->next;
 	}
+	free(head);
 }
