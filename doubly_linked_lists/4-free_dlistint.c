9af026299d90e5b24562d9e05cc7f674f8a1abfd
hedi <hedihamida98@gmail.com>
2022-07-23 12:44:20 +0100

    new

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,18 @@
 #include "lists.h"
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *next;
+	dlistint_t *prev;
+	next = malloc(sizeof(next));
 	while (head != NULL)
 	{
-		free(head->next);
-		if (head->prev != NULL)
+		next = head->next;
+		prev = head->prev;
+		free(next);
+		if (prev != NULL)
 		{
-			free(head->prev);
+			free(prev);
 		}
-
 		free(head);
 		head = head->next;
 	}
