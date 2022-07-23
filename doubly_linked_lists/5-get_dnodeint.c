ea2598725e4d034e4ba628ebc6bc1a8097a2a454
hedi <hedihamida98@gmail.com>
2022-07-23 13:34:49 +0100

    new

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,19 +3,19 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	int flag = 0;
-
+	int idx = index;
 	if (head == NULL)
 		return (NULL);
 
-	while (head != NULL)
+	while (head->next != NULL)
 	{
-		if (index == 0)
+		if (idx == -1)
 		{
 			flag = 1;
 			break;
 		}
 		head = head->next;
-		index--;
+		idx--;
 	}
 	if (flag)
 		return (head);
