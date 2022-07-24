30154c81148d664d9c5bb354a4f1d4b27b0ab7b8
hedi <hedihamida98@gmail.com>
2022-07-24 20:55:48 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,42 +8,37 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *p = *h;
-	dlistint_t *node;
+	dlistint_t *node, *tmp, *p = *h;
 
 	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = n;
-	node->prev = NULL;
-	node->next = NULL;
-	if (*h == NULL)
+	if (node != NULL)
 	{
-		*h = node;
-		return (node);
-	}
-	if (idx == 0)
-	{
-		(*h)->prev = node;
-		node->next = (*h);
-		(*h) = node;
-		return (node);
-	}
-	while (idx != 0)
-	{
-		if (p == NULL)
-			return (NULL);
-
-		if (idx == 1)
+		node->n = n;
+		node->next = NULL;
+		if (idx == 0)
 		{
-			node->next = p->next;
-			node->prev = p;
-			p->next->prev = node;
-			p->next = node;
-			break;
+			node->next = (*h);
+			(*h) = node;
+			return (*h);
 		}
-		p = p->next;
-		idx--;
+		while (idx != 0)
+		{
+			if (p == NULL)
+			{
+				return (NULL);
+			}
+			if (idx == 1)
+			{
+				tmp = p->next;
+				p->next = node;
+				node->next = tmp;
+				break;
+			}
+			p = p->next;
+			idx--;
+		}
+		return (node);
 	}
-	return (node);
+
+	return (NULL);
 }
