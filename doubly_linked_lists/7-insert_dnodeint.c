0cc5d7ad162a835c3091c5b5b44eb01b96cfcb43
hedi <hedihamida98@gmail.com>
2022-07-24 12:04:45 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node->next = (*h);
 		(*h)->prev = node;
 		(*h) = node;
-		return node;
+		return *h;
 	}
 	while (p->next != NULL)
 	{
