5bf6aac301b5956d5eacc067830139be2e55ddb2
hedi <hedihamida98@gmail.com>
2022-07-24 22:13:49 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,12 +41,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		p = p->next;
 		idx--;
 	}
-	if (idx > 0 && p->next != NULL)
+	if (idx > 0)
 		return NULL;
 
 	if (p->next == NULL)
 	{
-
 		node->next = p;
 		node->prev = p->prev;
 		p->prev->next = node;
