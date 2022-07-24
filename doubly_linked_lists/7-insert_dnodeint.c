81c778f46ddf3b3a6635949af14dfe4d8c714ad2
hedi <hedihamida98@gmail.com>
2022-07-25 00:32:06 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -43,6 +43,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (len == idx)
 		return (add_dnodeint_end(h, n));
 
+	if (idx > len)
+		return (NULL);
+
 	while (p != NULL)
 	{
 		if (tmp == idx)
