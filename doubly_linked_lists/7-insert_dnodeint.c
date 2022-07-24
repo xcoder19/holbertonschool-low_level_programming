fce02a01b170f6f7d0899932ccd039c6d3be3e7d
hedi <hedihamida98@gmail.com>
2022-07-24 22:39:47 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,14 +9,14 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *p = *h;
-	dlistint_t *node, *tmp;
+	dlistint_t *node;
 
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
 	if (*h == NULL || h == NULL)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	if (idx == 0)
 	{
 		node->next = *h;
@@ -28,11 +28,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (idx == 0)
 		{
-			tmp = p->prev;
+			node->prev = p->prev;
 			p->prev->next = node;
 			p->prev = node;
 			node->next = p;
-			node->prev = tmp;
 			return (node);
 		}
 		p = p->next;
