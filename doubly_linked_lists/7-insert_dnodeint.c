79f70403773d1f10fcba556c9b1b14041295a2f3
hedi <hedihamida98@gmail.com>
2022-07-24 11:44:36 +0100

    new

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -9,8 +8,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return NULL;
 
 	node->n = n;
-	node->next = NULL;
-	node->prev = NULL;
 
 	while (p->next != NULL)
 	{
