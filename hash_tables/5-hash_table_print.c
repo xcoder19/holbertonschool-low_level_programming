4a6ec5fc92ae5d56c1b58c9936f5c700ab3ce720
hedi <hedihamida98@gmail.com>
2022-08-20 22:44:49 +0100

    new

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,21 +6,19 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *p;
 	unsigned long int i = 0;
 	unsigned long int size = ht->size;
+	char *delim = "";
 	if (ht == NULL)
 		return;
 	printf("{");
 	while (i < size)
 	{
 		p = ht->array[i];
-		if (ht->array[i] != NULL)
+
+		while (p != NULL)
 		{
-			if (i + 1 == size)
-				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-
-			else
-				printf("'%s': '%s' ,", ht->array[i]->key, ht->array[i]->value);
-
-			p = p->next;
+			printf("%s'%s': '%s'", delim, ht->array[i]->key, ht->array[i]->value);
+			(void)(delim = ","),
+				p = p->next;
 		}
 
 		i++;
