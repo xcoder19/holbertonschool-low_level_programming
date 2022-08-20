eeccc13fd606821617e35f14120204300047b5ea
hedi <hedihamida98@gmail.com>
2022-08-20 22:17:35 +0100

    new

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *p;
+	unsigned long int i = 0;
+	unsigned long int size = ht->size;
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < size)
+	{
+		p = ht->array[i];
+		if (ht->array[i] != NULL)
+		{
+			if (ht->array[i]->next == NULL)
+				printf("'%s : %s'", ht->array[i]->key, ht->array[i]->value);
+
+			else
+				printf("'%s : %s',", ht->array[i]->key, ht->array[i]->value);
+
+			p = p->next;
+		}
+
+		i++;
+	}
+	printf("}");
+}
