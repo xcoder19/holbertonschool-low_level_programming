14db1a90f3a60d6327a9fed238f15553b5406aaa
hedi <hedihamida98@gmail.com>
2022-08-20 23:37:22 +0100

    new

---
+++ hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *table;
+	shash_node_t **array;
+	long unsigned int i;
+	array = malloc(sizeof(shash_node_t *) * size);
+	table = malloc(sizeof(shash_table_t));
+
+	if (table == NULL || array == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = array;
+	i = 0;
+	while (i < size)
+	{
+		table->array[i] = NULL;
+		i++;
+	}
+	return (table);
+}
