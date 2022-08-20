0741bee7e5aa82641aa9b06ad0c28190e38a03e6
hedi <hedihamida98@gmail.com>
2022-08-20 13:58:50 +0100

    new

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	hash_node_t **array;
-	array = malloc(sizeof(hash_node_t *));
+	array = malloc(sizeof(hash_node_t *) * size);
 	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL || array == NULL)
