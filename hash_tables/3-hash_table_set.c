a938a88d61bbae70cb2e21d7eb502adb42871ce2
hedi <hedihamida98@gmail.com>
2022-08-20 21:14:08 +0100

    new

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,13 +1,20 @@
 
 
 #include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @key:key
+ * @value:value
+ * Return: 1 on success
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *p;
 	hash_node_t *node;
 	unsigned long pos = hash_djb2((unsigned char *)key);
-
+	if (key == NULL)
+		return (0);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
