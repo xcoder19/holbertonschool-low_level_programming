4a3438bac73a22dbaa527072dec78ad25c9fc589
hedi <hedihamida98@gmail.com>
2022-08-20 22:19:40 +0100

    new

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -15,10 +15,10 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			if (ht->array[i]->next == NULL)
-				printf("'%s : %s'", ht->array[i]->key, ht->array[i]->value);
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 
 			else
-				printf("'%s : %s',", ht->array[i]->key, ht->array[i]->value);
+				printf("'%s': '%s',", ht->array[i]->key, ht->array[i]->value);
 
 			p = p->next;
 		}
