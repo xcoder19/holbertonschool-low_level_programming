27ad38e986e34412094528c9c4430d523fa06ae1
hedi <hedihamida98@gmail.com>
2022-08-20 14:50:03 +0100

    new

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,12 @@
 
 
 #include "hash_tables.h"
+/**
+ * key_index - give the index of key
+ * @key: key
+ * @size: size of array
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = 0;
