94a608a6d0811a14bcf329baf6e0ef1c2f5e3469
hedi <hedihamida98@gmail.com>
2022-08-20 22:51:46 +0100

    new

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,14 +1,20 @@
 
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - print key value pairs
+ * @ht: hashtable
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *p;
 	unsigned long int i = 0;
-	unsigned long int size = ht->size;
+	unsigned long int size;
 	char *delim = "";
+
 	if (ht == NULL)
 		return;
+
+	size = ht->size;
 	printf("{");
 	while (i < size)
 	{
