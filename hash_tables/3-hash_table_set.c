dd95e5b7173cfc7ce0c377e9f24d61ffda27d1c3
hedi <hedihamida98@gmail.com>
2022-08-20 21:11:53 +0100

    new

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,13 +5,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *p;
+	hash_node_t *node;
 	unsigned long pos = hash_djb2((unsigned char *)key);
-	index = pos % ht->size;
 
-	hash_node_t *node = malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 
+	index = pos % ht->size;
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
