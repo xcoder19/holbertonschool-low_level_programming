120203f7eb6c72fff7eae6451ff5124c1b16f250
hedi <hedihamida98@gmail.com>
2022-08-20 21:53:31 +0100

    new

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+
+
+#include "hash_tables.h"
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *p = ht->array[index];
+
+	if (p == NULL)
+		return NULL;
+
+	while (p != NULL)
+	{
+		if (strcmp(p->key, key) == 0)
+		{
+			return p->value;
+		}
+		p = p->next;
+	}
+	return NULL;
+}
