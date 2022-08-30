7a341cd15689dc1fe16c72a91038bd140a8b0363
hedi <hedihamida98@gmail.com>
2022-08-30 21:50:59 +0100

    new

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void print_array(int *array, int start, int end);
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
